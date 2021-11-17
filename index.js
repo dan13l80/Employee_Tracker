402111bc6bf827dd93291b88806ab9f29225b391
dan13l80 <daniel.subramanian@outlook.com>
2021-11-17 02:32:42 -0500

    Depend added

---
+++ index.js
@@ -0,0 +1,5 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const view = require('./utils/view.js');
+const add = require('./utils/add.js');
+const update = require('./utils/update.js');
