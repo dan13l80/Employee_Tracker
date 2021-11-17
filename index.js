f90118530029398cf100dd3f40e9f22bb4c8dede
dan13l80 <daniel.subramanian@outlook.com>
2021-11-17 02:33:36 -0500

    connection to db added

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const inquirer = require('inquirer');
 const view = require('./utils/view.js');
 const add = require('./utils/add.js');
 const update = require('./utils/update.js');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "company_DB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+});
